839038f8ddac6affec36fd794c6f5c2326fe9663
soumanpaul <souman1729@gmail.com>
2025-02-09 23:54:04 +0530

    added search page

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,7 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
+import { useLocalSearchParams } from 'expo-router'
+
 export default function Search() {
+    const { query } = useLocalSearchParams();
+
   return (
-    <div>Search</div>
+    <SafeAreaView className='bg-primary h-full'>
+        <Text className='text-3xl text-white'>{query}</Text>
+    </SafeAreaView>
   )
 }
